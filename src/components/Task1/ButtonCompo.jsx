c808276e6751d531c63ac873166822af18813fec
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-10 19:25:35 +0545

    fixed issue with syntax

--- src/components/Task1/ButtonCompo.jsx
+++ src/components/Task1/ButtonCompo.jsx
@@ -9,13 +9,13 @@ const ButtonCompo = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="3"
+          strokeWidth="3"
           stroke="currentColor"
           className="size-4 text-[#2B2B2B] transition-transform duration-300 group-hover:translate-x-1 arrow-pulse"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
           />
         </svg>
