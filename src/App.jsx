46d1e39f43f42a8c0f86acbd84aa0b001f281ead
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-12 14:07:04 +0545

    task2 view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Task1 from './pages/Task1'
+import Task2 from './pages/Task2'
 
 const App = () => {
   return (
     <div>
-      <Task1/>
+      {/* <Task1/> */}
+      <Task2/>
     </div>
   )
 }
