8b45b468130cf795672accc35a66a5027bf3600b
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-12 14:06:07 +0545

    Heading for task 1

---
+++ src/components/Task2/Heading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div>
+      <p className="text-black font-[outfit] text-2xl leading-[100%] tracking-normal mb-6">
+        Your Skillshikshya Journey{" "}
+      </p>
+      <p className="font-[Poppins] font-bold text-3xl leading-[120%] tracking-normal">
+        <span className="text-[#37a077]">Step</span> In.{" "}
+        <span className="text-[#37a077]">Skill</span> Up.{" "}
+        <span className="text-[#37a077]">Stand</span> Out. 🚀
+      </p>
+    </div>
+  );
+};
+
+export default Heading;
